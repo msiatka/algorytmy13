b7fc424745a073ae1a0a4ee597b3f67b977952e6
Marcin Siatka <siatka131@gmail.com>
2018-12-26 18:13:27 +0100

    test2

--- src/pl/sda/algorytmy13/algorytmy/Bag.java
+++ src/pl/sda/algorytmy13/algorytmy/Bag.java
@@ -1,4 +1,60 @@
 package pl.sda.algorytmy13.algorytmy;
 
-public class Bag {
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class Bag<Item> implements Iterable<Item>{
+
+
+    private Node first;  //pierwszy węzeł na liście
+    private int N;
+
+
+    private class Node
+    {
+        Item item;
+        Node next;
+    }
+
+    public void Bag() {
+
+    }
+
+    public void add(Item item) {
+        //to samo co w metodzie push klasy Stack
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+    }
+
+    public boolean isEmpty() {
+        return first==null; // lub N =0
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public Iterator<Item> iterator(){
+        return new ListIterator();
+    }
+
+    private class ListIterator implements Iterator<Item>
+    {
+        private Node current = first;
+        public boolean hasNext()
+        {return current != null;}
+
+        public void remove(){}
+
+        public Item next()
+        {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+
 }
--- src/pl/sda/algorytmy13/algorytmy/Stats.java
+++ src/pl/sda/algorytmy13/algorytmy/Stats.java
@@ -1,4 +1,32 @@
 package pl.sda.algorytmy13.algorytmy;
 
+import java.util.Scanner;
+
 public class Stats {
+    public static void main(String[] args){
+        Bag<Double> numbers = new Bag<Double>();
+        Scanner stdin = new Scanner(System.in);
+
+        System.out.println(" podaj liczbe, 0 konczy program ");
+         double y;
+        y=1;
+        while(y!=0) {
+            y=stdin.nextDouble();
+            numbers.add(y);
+        }
+        int N = numbers.size();
+
+        double sum = 0.0;
+        for(double x : numbers)
+            sum+=x;
+        double mean = sum/N;
+
+        sum =0.0;
+        for(double x: numbers)
+            sum+=(x-mean)*(x-mean);
+        double std = Math.sqrt(sum/(N-1));
+
+        System.out.println("Srednia %.2f\n" + mean);
+        System.out.println(" Odch. st.: %.2f\n" + std);
+    }
 }
